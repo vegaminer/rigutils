c4df33ebc5b137898331f2a14dd4a38126ee4071
Keksov <keksov@gmail.com>
2019-01-16 11:05:44 +0300

    Introduction of BlockWindowsUpdate.bat

--- windows_tuning/BlockWindowsUpdate.bat
+++ windows_tuning/BlockWindowsUpdate.bat
@@ -28,41 +28,44 @@ rem https://github.com/vFense/vFenseAgent-win/wiki/Registry-keys-for-configuring
 rem https://www.windowscentral.com/how-stop-updates-installing-automatically-windows-10
 
 rem Creating registry entries
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f
+set "WU_KEY=HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+set "AU_KEY=%WU_KEY%\AU"
+
+"%reg%" ADD %WU_KEY% /f
+"%reg%" ADD %AU_KEY% /f
 
 rem 5 = Automatic Updates is required and users can configure it.
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 5 /f
+"%reg%" ADD %AU_KEY% /v AUOptions /t REG_DWORD /d 5 /f
 
 rem 1 = Disable Automatic Updates.
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 1 /f
+"%reg%" ADD %AU_KEY% /v NoAutoUpdate /t REG_DWORD /d 1 /f
 
 rem 1 = The computer gets its updates from a WSUS server. We are providing fake address for it
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v UseWUServer /t REG_DWORD /d 1 /f
+"%reg%" ADD %AU_KEY% /v UseWUServer /t REG_DWORD /d 1 /f
 
 rem Fake address for WindowsUpdate service
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v WUServer /t REG_SZ /d "http://127.0.0.2" /f
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v WUStatusServer /t REG_SZ /d "http://127.0.0.2" /f
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v UpdateServiceUrlAlternate /t REG_SZ /d "http://127.0.0.2" /f
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v FillEmptyContentUrls /t REG_DWORD /d 1 /f
+"%reg%" ADD %WU_KEY% /v WUServer /t REG_SZ /d "http://127.0.0.2" /f
+"%reg%" ADD %WU_KEY% /v WUStatusServer /t REG_SZ /d "http://127.0.0.2" /f
+"%reg%" ADD %WU_KEY% /v UpdateServiceUrlAlternate /t REG_SZ /d "http://127.0.0.2" /f
+"%reg%" ADD %WU_KEY% /v FillEmptyContentUrls /t REG_DWORD /d 1 /f
 
-rem Telling WU never call home
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v DoNotConnectToWindowsUpdateInternetLocations /t REG_DWORD /d 1 /f
+rem Telling WindowsUpdate never call home
+"%reg%" ADD %WU_KEY% /v DoNotConnectToWindowsUpdateInternetLocations /t REG_DWORD /d 1 /f
 
-rem Never try to update device drivers via WU
-"%reg%" ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d 1 /f
+rem Never try to update device drivers via WindowsUpdate
+"%reg%" ADD %WU_KEY% /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d 1 /f
 
-rem Stopping and disabling Windows Update related services
+rem Stopping and disabling WindowsUpdate related services
 
 rem Update Orchestrator Service
 sc stop UsoSvc >nul 2>&1
 sc config UsoSvc start=disabled >nul 2>&1
 
-rem Windows Update
+rem WindowsUpdate
 sc stop wuauserv >nul 2>&1
 sc config wuauserv start=disabled >nul 2>&1
 
-rem Windows Update Medic Service
+rem WindowsUpdate Medic Service
 sc stop WaaSMedicSvc >nul 2>&1
 sc config WaaSMedicSvc start=disabled >nul 2>&1
 
